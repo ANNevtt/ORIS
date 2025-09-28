22bae93f77d30e5a87b5a09688717ff2e00dca57
Anna <aniutik2006@gmail.com>
2025-09-29 00:17:15 +0400

    Add files via upload

---
+++ 06.09/index.js
@@ -0,0 +1,26 @@
+document.getElementById('themeToggle').addEventListener('click', function() {
+    const currentTheme = document.body.className;
+    if (currentTheme === 'light-theme') {
+        document.body.className = 'dark-theme';
+    } else {
+        document.body.className = 'light-theme';
+    }
+});
+
+const button = document.querySelector('.button')
+const menu = document.querySelector('.menu')
+const menuLinks = document.querySelectorAll('.menu-link')
+
+button.addEventListener('click', (e) => {
+  button.classList.toggle('active')
+
+  if (button.classList.contains('active')) {
+    button.setAttribute('aria-expanded', 'true')
+    menu.setAttribute('aria-hidden', 'false')
+    menuLinks.forEach(link => link.setAttribute('tabindex', '0'))
+  } else {
+    button.setAttribute('aria-expanded', 'false')
+    menu.setAttribute('aria-hidden', 'true')
+    menuLinks.forEach(link => link.setAttribute('tabindex', '-1'))
+  }
+})
